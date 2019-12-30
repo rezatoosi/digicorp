f802043668f69fd5172088b8b93815f533552121
Reza Khoshghalb <reza.toosi@gmail.com>
2019-12-30 15:55:42 +0330

    change slidertyped to notyped

---
+++ template-parts/sections/frontpage-notypedslider2.php
@@ -0,0 +1,57 @@
+
+      <?php
+      if ( get_theme_mod( 'digicorp_typed_slider_enabled', 1 ) ):
+      ?>
+        <section class="visual visual-2" id="typed-slider">
+            <div class="container">
+                <div class="text-block">
+                    <div class="heading-holder">
+                        <h1><?php echo get_theme_mod( 'digicorp_typed_slider_title' ); ?></h1>
+                    </div>
+                    <div>
+                      <p>
+                      <?php echo get_theme_mod( 'digicorp_typed_slider_typed_elements' ) ?>
+                      </p>
+                    </div>
+                    <div class="infos">
+                      <?php
+                        $info_tags = explode( "\n", get_theme_mod( 'digicorp_typed_slider_info_tags' ) );
+                        foreach ( $info_tags as $info_tag ) {
+                          echo "<span class=\"info\"><i class=\"fa fa-star\"></i> $info_tag</span>\n";
+                        }
+                      ?>
+                    </div>
+                </div>
+            </div>
+            <?php
+            if ( ! empty( get_theme_mod( 'digicorp_typed_slider_background' ) ) ) {
+              printf(
+                '<img src="%1$s" alt="%2$s" class="bg-stretch" id="typed-slider-bg">',
+                get_theme_mod( 'digicorp_typed_slider_background' ),
+                get_theme_mod( 'digicorp_typed_slider_title' )
+              );
+            }
+            // else {
+            //   printf(
+            //     '<img src="%1$s" alt="%2$s" class="bg-stretch" id="typed-slider-bg">',
+            //     get_template_directory_uri() . '/assets/dist/images/slider-bg.jpg',
+            //     get_theme_mod( 'digicorp_typed_slider_title' )
+            //   );
+            // }
+            ?>
+        </section>
+
+        <!-- call to action -->
+        <?php if ( ! empty( get_theme_mod( 'digicorp_typed_slider_cta_text' ) ) ): ?>
+        <section class="section nopad cta" id="slider-cta">
+            <div class="container nopad">
+                <div id="cta">
+                    <a href="<?php echo esc_url( get_page_link( get_theme_mod( 'digicorp_typed_slider_cta_link' ) ) ); ?>" class="btn btn-primary rounded"><?php echo get_theme_mod( 'digicorp_typed_slider_cta_text' ); ?></a>
+                </div>
+            </div>
+        </section>
+        <?php endif; ?>
+
+      <?php
+      endif;
+      ?>
