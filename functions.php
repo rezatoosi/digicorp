6815c8e4b04415b4e4eced7b6caa3ddfaf449e73
Reza Khoshghalb <reza.toosi@gmail.com>
2020-11-18 18:04:23 +0330

    remove w3 total cache comment in bottom of pages html

--- functions.php
+++ functions.php
@@ -131,4 +131,7 @@
     add_filter( 'ariana_widget_box_icon_con_class_default', function() { return 'box-icon'; } );
     add_filter( 'ariana_widget_box_content_con_class_default', function() { return 'box-content'; } );
 
+    // remove w3 total cache comments form bottom of pages html
+    add_filter( 'w3tc_can_print_comment', function( $w3tc_setting ) { return false; }, 10, 1 );
+
 ?>
