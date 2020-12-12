c2f4007269e38caf03e3fc5db5155a1cd87e51ee
Reza Khoshghalb <reza.toosi@gmail.com>
2020-12-12 17:59:09 +0330

    bit fix in register scripts

--- inc/register-scripts.php
+++ inc/register-scripts.php
@@ -1,7 +1,7 @@
 <?php
 
 function digicorp_remove_jquery() {
-  if ( ! is_admin() ) {
+  if ( ! is_admin() && ! is_customize_preview() ) {
     wp_deregister_script('jquery');
   }
 }
@@ -9,7 +9,7 @@ add_action( 'wp_enqueue_scripts', 'digicorp_remove_jquery' );
 
 //Remove JQuery migrate
 function digicorp_remove_jquery_migrate( $scripts ) {
-  if (!is_admin() && isset($scripts->registered['jquery'])) {
+  if ( ( ! is_admin() && ! is_customize_preview() ) && isset( $scripts->registered['jquery'] )) {
     $script = $scripts->registered['jquery'];
     if ($script->deps) { // Check whether the script has any dependencies
       $script->deps = array_diff($script->deps, array(
@@ -21,6 +21,7 @@ function digicorp_remove_jquery_migrate( $scripts ) {
 add_action('wp_default_scripts', 'digicorp_remove_jquery_migrate');
 
 function digicorp_dequque_wp_embed(){
+  if ( is_admin() || is_customize_preview() ) return;
   wp_deregister_script( 'wp-embed' );
 }
 add_action( 'wp_enqueue_scripts', 'digicorp_dequque_wp_embed' );
@@ -109,7 +110,6 @@ function ariana_disable_emoji_feature() {
 	 */
 
 	add_filter( 'option_use_smilies', '__return_false' );
-
 }
 
 function ariana_disable_emojis_tinymce( $plugins ) {
@@ -118,5 +118,4 @@ function ariana_disable_emojis_tinymce( $plugins ) {
 	}
 	return $plugins;
 }
-
 add_action('init', 'ariana_disable_emoji_feature');
