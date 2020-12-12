364f55d33e90f24c0242f167ed70e1e6ac1c5cfa
Reza Khoshghalb <reza.toosi@gmail.com>
2020-12-12 16:24:23 +0330

    bit fix in digicorp_the_post_navigation() - but important for search query urls

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -823,7 +823,7 @@ if ( ! function_exists( 'digicorp_the_posts_navigation' ) ) :
     global $wp_query;
     $big = 999999999;
     $pagination = array(
-      'base'               => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+      'base'               => str_replace( $big, '%#%', html_entity_decode( get_pagenum_link( $big ) ) ),
       'format'             => '?paged=%#%',
       'total'              => $wp_query->max_num_pages,
       'current'            => max( 1, get_query_var('paged') ),
