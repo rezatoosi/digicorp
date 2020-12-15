99152afe2a90f62c7788025d44d078e91d862b44
Reza Khoshghalb <reza.toosi@gmail.com>
2020-12-16 00:11:46 +0330

    changes in output html of projects box - SEO reasons

--- template-parts/projects/projects-excerpt.php
+++ template-parts/projects/projects-excerpt.php
@@ -1,15 +1,4 @@
 <?php
-if ( has_post_thumbnail() ) {
-  $img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'small' )[0];
-} else {
-  $img_url = digicorp_get_projects_default_image_uri();
-}
-$img_markup = sprintf(
-  '<img src="%s" alt="%s">',
-  $img_url,
-  $post->post_title
-);
-
 $portfo = '';
 if ( has_term( '', 'ariana-portfolio' ) ) {
   $portfo = get_the_terms( $post->ID, 'ariana-portfolio' );
@@ -25,9 +14,20 @@ if ( has_term( '', 'ariana-portfolio' ) ) {
   $portfo = __( 'Web Services', 'digicorpdomain' );
 }
 
+if ( has_post_thumbnail() ) {
+  $img_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'small' )[0];
+} else {
+  $img_url = digicorp_get_projects_default_image_uri();
+}
+$img_markup = sprintf(
+  '<img src="%s" alt="%s">',
+  $img_url,
+  $portfo . ' ' . $post->post_title
+);
+
 ?>
 <article class="project-box item">
-  <a href="<?php the_permalink() ?>" title="<?php the_title() ?>">
+  <a href="<?php the_permalink() ?>" title="<?php echo $portfo . ' ' . $post->post_title ?>">
     <figure>
       <?php echo $img_markup ?>
       <figcaption class="project-box__content">
